09343d998d4a01d5bbc1e3d3261d0986b4376dd4
Axel <axldotm@gmail.com>
2018-10-11 19:44:39 +0100

    first commit.

---
+++ trajectory_viewer/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(
+                "_____ _____ _____    __ _____ _____ _____ _____ _____ __ __    _____ _____ _____ _ _ _ _____ _____ \n" +
+                        "|_   _| __  |  _  |__|  |   __|     |_   _|     | __  |  |  |  |  |  |     |   __| | | |   __| __  |\n" +
+                        "  | | |    -|     |  |  |   __|   --| | | |  |  |    -|_   _|  |  |  |-   -|   __| | | |   __|    -|\n" +
+                        "  |_| |__|__|__|__|_____|_____|_____| |_| |_____|__|__| |_|     \\___/|_____|_____|_____|_____|__|__|\n" +
+                        "                                                                                                    ");
+        System.out.println("Trajectory viewer.\n" +
+                "This program displays the selected animal trajectory from the mongoDB database.");
+        System.out.println("Axel Montout @ University of bristol. Copyright 2018.\n");
+    }
+}
